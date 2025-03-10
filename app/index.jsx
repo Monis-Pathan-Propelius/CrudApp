f9717bb3548c9f4542650605e2594a6ad783ad96
Monis <monis.pathan@propelius.tech>
2025-03-10 14:08:38 +0530

    -Added Dark and light theming option -Added simple Crud functions to simulate simple CRUD Apis

--- app/index.jsx
+++ app/index.jsx
@@ -10,42 +10,132 @@ import {
   TextInput,
   View,
 } from "react-native";
+
 import { SafeAreaView } from "react-native-safe-area-context";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 import { ToDoListData } from "@/data/todos";
+import { useState, useContext, useEffect } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
+import { StatusBar } from "expo-status-bar";
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+
+import Octicons from "@expo/vector-icons/Octicons";
+
+import Animated, { LinearTransition } from "react-native-reanimated";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Index() {
-  const styles = Styles();
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+
+  const { colorScheme, setColorScheme, theme } = useContext(ThemeContext);
+
+  const styles = Styles(theme, colorScheme);
+  const [loaded, error] = useFonts({ Inter_500Medium });
 
   const { width } = Dimensions.get("window");
   const TabletSize = width >= 760;
 
   const Container =
     Platform.OS === "android" || "ios" ? SafeAreaView : ScrollView;
+
+  const AddToDo = () => {
+    if (text.trim()) {
+      const newId = toDo.length > 0 ? toDo[0].id + 1 : 1;
+
+      setToDo([{ id: newId, title: text, completed: false }, ...toDo]);
+
+      setText("");
+    }
+  };
+
+  const toggleTodo = (id) => {
+    setToDo(
+      toDo.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const removeToDo = (id) => {
+    setToDo(toDo.filter((todo) => todo.id !== id));
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("TodoApp");
+        const storageToDos = jsonValue != null ? JSON.parse(jsonValue) : null;
+
+        if (storageToDos && storageToDos.length > 0) {
+          setToDo(storageToDos.sort((a, b) => b.id - a.id));
+        } else {
+          setToDo(ToDoListData.sort((a, b) => b.id - a.id));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [ToDoListData]);
+
+  useEffect(() => {
+    const storeData = async () => {
+      try {
+        const jsonValue = JSON.stringify(toDo);
+        await AsyncStorage.setItem("TodoApp", jsonValue);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    storeData();
+  }, [toDo]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <Container
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <FlatList
+    <Container style={styles.container}>
+      <Animated.FlatList
         style={[styles.FlatlistMobile, TabletSize && styles.FlatlistBigScreen]}
-        data={ToDoListData}
+        data={toDo}
         showsVerticalScrollIndicator={false}
         ListHeaderComponent={
           <View style={styles.Header}>
-            <TextInput style={styles.AddToDoText} />
-            <Pressable
-              style={({ pressed }) => [
-                styles.AddBtn,
-                pressed && styles.btnPressed,
-              ]}
-            >
-              <Text>Add</Text>
-            </Pressable>
+            <TextInput
+              value={text}
+              style={styles.AddToDoText}
+              onChangeText={(value) => setText(value)}
+            />
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Pressable
+                style={({ pressed }) => [
+                  styles.AddBtn,
+                  pressed && styles.btnPressed,
+                ]}
+                onPress={() => AddToDo()}
+              >
+                <Text style={styles.AddBtnText}>Add</Text>
+              </Pressable>
+              <Pressable
+                onPress={() =>
+                  setColorScheme(colorScheme === "light" ? "dark" : "light")
+                }
+                style={{ marginLeft: 10 }}
+              >
+                <Octicons
+                  name={colorScheme === "dark" ? "moon" : "sun"}
+                  size={36}
+                  color={theme.text}
+                  selectable={undefined}
+                  style={{ width: 36 }}
+                />
+              </Pressable>
+            </View>
           </View>
         }
         ListFooterComponent={
@@ -55,36 +145,57 @@ export default function Index() {
         renderItem={({ item }) => (
           <View style={styles.MainList}>
             <View style={styles.ListItem}>
-              <Text>{item.title}</Text>
+              <Text
+                style={[
+                  styles.toDoText,
+                  item.completed && styles.completedText,
+                ]}
+                onPress={() => toggleTodo(item.id)}
+              >
+                {item.title}
+              </Text>
               <Pressable
                 style={({ pressed }) => [
                   styles.DeleteBtn,
                   pressed && styles.btnPressed,
                 ]}
+                onPress={() => removeToDo(item.id)}
               >
                 <FontAwesome5 name="trash" size={15} color="black" />
               </Pressable>
             </View>
           </View>
         )}
+        itemLayoutAnimation={LinearTransition}
+        keyboardDismissMode="on-drag"
       />
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
     </Container>
   );
 }
 
-const Styles = () => {
+const Styles = (theme, colorScheme) => {
   return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+    },
     FlatlistMobile: {
       width: "100%",
+      marginHorizontal: "auto",
     },
     FlatlistBigScreen: {
-      width: "20rem",
+      width: "25rem",
+      marginHorizontal: "auto",
     },
     Header: {
       flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
       gap: 7,
       padding: 6,
     },
+
     HeaderButton: {
       flex: "1",
       color: "red",
@@ -104,7 +215,8 @@ const Styles = () => {
     },
     AddBtn: {
       padding: 8,
-      backgroundColor: "white",
+
+      backgroundColor: theme.button,
       borderRadius: 4,
       shadowColor: "#000",
       shadowOffset: { width: 0, height: 4 },
@@ -115,6 +227,11 @@ const Styles = () => {
       alignItems: "center",
       paddingHorizontal: 10,
     },
+    AddBtnText: {
+      fontSize: 18,
+      color: colorScheme === "dark" ? "black" : "white",
+    },
+
     DeleteBtn: {
       padding: 10,
       backgroundColor: "red",
@@ -124,7 +241,7 @@ const Styles = () => {
       height: 1,
       width: "100%",
       marginHorizontal: "auto",
-      backgroundColor: "black",
+      backgroundColor: theme.text,
     },
     listFooter: {
       width: "100%",
@@ -134,9 +251,23 @@ const Styles = () => {
       backgroundColor: "#858282",
     },
     AddToDoText: {
-      flexGrow: 1,
+      flex: 1,
       borderWidth: 1,
-      backgroundColor: "white",
+      backgroundColor: theme.background,
+      padding: 5,
+      fontFamily: "Inter_500Medium",
+      color: theme.text,
+      borderColor: theme.text,
+    },
+
+    toDoText: {
+      fontSize: 18,
+      color: theme.text,
+      fontFamily: "Inter_500Medium",
+    },
+    completedText: {
+      textDecorationLine: "line-through",
+      opacity: 0.5,
     },
   });
 };
